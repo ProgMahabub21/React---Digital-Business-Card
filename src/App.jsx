5d9f115905ec2d3c8a707cf5b9c2b582c9e6d111
ProMahabub21 <imahbub.1999@gmail.com>
2023-02-10 12:54:32 +0600

    project Setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Form from './components/form';
+export default function App() {
+    return (
+        <div>
+           <Form /> 
+        <div>Hello World</div>
+        </div>
+    );
+}
