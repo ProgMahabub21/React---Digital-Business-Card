bb7d074d8ad11cba25ed6778ed2b7ddd5c92d73b
ProMahabub21 <imahbub.1999@gmail.com>
2023-02-10 23:34:29 +0600

    Styling part 2 done

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,31 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { SocialIcon } from "react-social-icons";
+import { SocialIcon,  } from "react-social-icons";
+
+
 
 
 export default function Body() {
 
     return(
       <main>
-            <h1 className="body--title">MD Mahabub</h1>
-            <p>Software Developer</p>
+            <h1 className="body--title">MD Mahabub Islam</h1>
+            <p className="designation">Frontend Developer</p>
             {/* <p>Web Developer</p> */}
-            <p>Website link</p>
+            <p className="weblink">progmahabub21.github.io/My-Website/</p>
 
             <div>
-                <button className="button">Email</button>
-                <button className="lkd"><SocialIcon url="https://www.linkedin.com/in/md-mahabub-5b3853177/" />Linkedin</button>
+                <button className="button" onClick={() => window.location = 'mailto:imahbub.1999@gmail.com'}>
+                    <SocialIcon network="email" bgColor="white" fgColor="black" style={{ height: 30, width: 30 }}/>Email</button>
+                <button className="lkd"><SocialIcon url="https://www.linkedin.com/in/md-mahabub-5b3853177/" 
+                style={{ height: 25, width: 25 }} bgColor="white"  />Linkedin</button>
             </div>
 
             <div>
-                <h3>About</h3>
-                <p>I am a FrontEnd Developer. I am learning React js and other frameworks for my developement skill purpose.
-                I am also interested in Full-Stack web development with Nodejs. I try to develop my projects with best practices and 
-                following standards. I am passionate about new things to learn. I am also a researcher in Blockchain Technology field.
-                I hold Bachelors degree in Computer Science & Engineering (CSE), American International University-Bangladesh. </p>
+                <h3 className="about--title">About</h3>
+                <p className="about--content">I am a Frontend Developer. I am learning React, a popular js library and Next.js for my developement skill purpose.
+                I am highly interested in Full-Stack Web development with Js. I try to develop my projects with best practices and 
+                following standards. I love learning new things. I am also a researcher in Blockchain Technology field.
+                I are persuing my Bachelors degree in Computer Science & Engineering (CSE) from American International University-Bangladesh. </p>
 
-                <h3>Interests</h3>
-                <p>Food expert, Home cook, Reader, Researcher, Internet fanatic, Digital video gamer, News Seeker, Foody </p>
+                <h3 className="interest--title">Interests</h3>
+                <p className="interest--content">Food expert, Home cook, Reader, Researcher, Internet fanatic, Digital video gamer, News Seeker, Foody </p>
                 
             </div>
         </main>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,16 +8,20 @@ export default function Footer() {
     <footer>   
                   
                    <div className="footer--social">     
-                        <SocialIcon url="https://www.facebook.com/md.mahabub.581" className='facebook' />
+                        <SocialIcon url="https://www.facebook.com/md.mahabub.581" style={{width : 32, height: 32}} className='facebook' />
                         
-                        <SocialIcon url="https://twitter.com/mdmahabub_1" className='twiiter'/>
+                        <SocialIcon url="https://twitter.com/mdmahabub_1" style={{width : 32, height: 32}} className='twiiter'/>
                        
-                        <SocialIcon url="https://www.instagram.com/mdmahabub_1/" className='insta' />
+                        <SocialIcon url="https://www.instagram.com/mdmahabub_1/" style={{width : 32, height: 32}} className='insta' />
                         
-                        <SocialIcon url="https://www.github.com/ProgMahabub21" className='github'/>
-                   </div>
+                        <SocialIcon url="https://www.github.com/ProgMahabub21" style={{width : 32, height: 32}} className='github'/>
 
+                        
+                   </div>
+                   <div>
                     <p className="footer--license">© 2023 MD Mahabub</p> 
+                   </div>
+                   
                 
     </footer>
 
